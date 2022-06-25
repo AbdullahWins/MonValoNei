5c6988998084ce0aa48e0770f9305d4e90a5c2e2
Abdullah Al MahMud <33630553+AbdullahWins@users.noreply.github.com>
2022-06-25 18:20:28 +0600

    version 1.0
    First commit and probably the last.

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'monVaoNei';
+  public mon = "";
+  public counter = 0;
+  public monKharap() {
+    if (this.counter < 15) {
+    this.mon = this.mon + "  >_< আজকে আমার মন ভালো নেই >_<  ";
+    this.counter = this.counter + 1;
+    }
+    else {
+      this.mon = " -_- তোর মন খারাপের মারে সুদি -_-";
+    }
+  }
+  public reset() {
+    this.mon = "";
+    this.counter = 0;
+  }
+}
